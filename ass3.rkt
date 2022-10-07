379c932cbe3ddd522751cac8d249008ceeaac45e
Walter Toriola <walte@Walters-MBP.lan>
2022-10-06 17:33:38 -0700

    No more plusC multC when parsing

--- ass3.rkt
+++ ass3.rkt
@@ -5,34 +5,44 @@
 ;TODO
 ;Will need to genericize ArithC to ExprC
 ;Will need to parse with {} instead of ()
-;Will need to handle what happens with 'main'
+;Will need to handle what happens with 'main
 ;Will need to add function definition logic
 ;Will need to create mapping function for binary operations
 ;Will need to create other functions e.g top-interp
 
 ;Arith language top-definitons
-(define-type ArithC (U numC plusC multC leq0?))
+(define-type ArithC (U numC binop leq0?))
 (struct numC ([n : Real])#:transparent)
-(struct plusC ([l : ArithC] [r : ArithC])#:transparent)
-(struct multC ([l : ArithC] [r : ArithC])#:transparent)
+;(struct plusC ([l : ArithC] [r : ArithC])#:transparent)
+;(struct multC ([l : ArithC] [r : ArithC])#:transparent)
+(struct binop ([operator : Sexp] [l : ArithC] [r : ArithC])#:transparent)
 (struct leq0? ([test : ArithC] [then : ArithC] [else : ArithC])#:transparent)
 
+
+;function that maps binop symbol to arithetic operation
+(define (arithmetic_operators [s : Sexp] [l : Real] [r : Real]) : Real
+  (match s
+    ['+ (+ l r)]
+    ['* (* l r)])
+  )
 
 ;evaluator/interpreter
 (define (interp [target : ArithC]) : Real
   (match target
       [(numC n) n]
-      [(plusC l r) (+ (interp l) (interp r))]
-      [(multC  l r) (* (interp l) (interp r))]
+      ;[(plusC l r) (+ (interp l) (interp r))]
+      ;[(multC  l r) (* (interp l) (interp r))]
       [(leq0? a b c) (if (<= (interp a) 0) (interp b) (interp c))]
+      [(binop o l r) (arithmetic_operators o (interp l) (interp r))]
       [else (error 'JYSS "unimplemented")]
     ))
 
 ;test-case
-(check-equal? (interp (plusC (numC 5) (numC 5))) 10)
-(check-equal? (interp (multC (numC 5) (numC 5))) 25)
-(check-equal? (interp (leq0? (numC 4) (numC 4) (numC 4))) 4)
-(check-equal? (interp (leq0? (numC -1) (numC -1) (numC -1))) -1)
+;(check-equal? (interp (plusC (numC 5) (numC 5))) 10)
+;(check-equal? (interp (multC (numC 5) (numC 5))) 25)
+(check-equal? (interp (binop '+ (binop '* (numC 1) (numC 2)) (binop '+ (numC 2) (numC 3)))) 7)
+(check-equal? (interp (leq0? (numC 4) (numC 4) (numC 800))) 800)
+(check-equal? (interp (leq0? (numC -1) (numC 7) (numC -1))) 7)
 
 ;parser
  (define (parse [s : Sexp]) : ArithC
@@ -40,15 +50,14 @@
       [(real? s) (numC s)]
       [(list? s)
        (case (first s)
-         [(+) (plusC (parse (second s)) (parse (third s)))]
-         [(*) (multC (parse (second s)) (parse (third s)))]
+         [(+ *) (binop (first s) (parse (second s)) (parse (third s)))]
          [(leq0?) (leq0? (parse (second s)) (parse (third s)) (parse (fourth s)))]
          [else (error 'JYSS "invalid list input")])]
       [else (error 'JYSS "invalid input")]))
 
 ;test-case
-(check-equal? (parse '(+ (* 1 2) (+ 2 3))) (plusC (multC (numC 1) (numC 2))(plusC (numC 2) (numC 3))))
-(check-equal? (parse '(leq0? 4 5 5)) (leq0? (numC 4) (numC 5) (numC 5)))
+(check-equal? (parse '{+ {* 1 2} {+ 2 3}}) (binop '+ (binop '* (numC 1) (numC 2)) (binop '+ (numC 2) (numC 3))))
+(check-equal? (parse '{leq0? 4 5 5}) (leq0? (numC 4) (numC 5) (numC 5)))
 
 
 
